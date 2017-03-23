3f680962b3809ca5f9c693509813b06d72216fd9
plusIs <plusls@qq.com>
2017-03-24 04:16:53 +0800

    完成 printf

---
+++ cnss/sayagain/repeat.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+void getflag()
+{
+    system("/bin/sh");
+}
+
+int main()
+{
+    char str[1024];
+    memset(str, 0, sizeof(char) * 1024);
+    puts("This is a program,which can repeat what you said");
+    puts("input something:");
+    read(0, str, 1023);
+    puts("Haha, your input is:");
+    printf(str);
+    putchar('\n');
+    //printf("%08x\n", *(int*)(0x0804a01c));    
+    printf("the length of your input is %d \n", strlen(str));
+    return 0;
+}
